7fa62632dc40e84914ee955c7860f0d84399374f
JuanRuano1247723 <123260280+JuanRuano1247723@users.noreply.github.com>
2023-05-03 19:16:00 -0600

    Add files via upload

---
+++ Semana 8/Ejercicio3semana8CajaRegistradora/Ejercicio3semana8CajaRegistradora/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace Ejercicio3semana8CajaRegistradora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Daniel Ruano 1247723
+            //Angel Pérez 1135323
+            int numBilletes200 = 0,
+                     numBilletes100 = 0,
+                     numBilletes50 = 0,
+                     numBilletes20 = 0,
+                     numBilletes10 = 0,
+                     numBilletes5 = 0,
+                     numMonedas1 = 0,
+                     numMonedas50c = 0,
+                     numMonedas25c = 0,
+                     numMonedas10c = 0, 
+                     numMonedas5c = 0;
+            double totalCaja;
+
+        inicio:
+            Console.Clear();
+            Console.WriteLine("Ingrese la cantidad de la caja: ");
+            bool ni = double.TryParse(Console.ReadLine(), out  totalCaja);
+            if (ni == true && totalCaja >= 0)
+            {
+                do
+                {
+                    if (totalCaja % 200 == 0)
+                    {
+                        numBilletes200++;
+                        totalCaja -= 200;
+                    } else if (totalCaja % 100 == 0)
+                    {
+                        numBilletes100++;
+                        totalCaja -= 100;
+                    }
+                    else if (totalCaja % 50 == 0)
+                    {
+                        numBilletes50++;
+                        totalCaja -= 50;
+                    }
+                    else if (totalCaja % 20 == 0)
+                    {
+                        numBilletes20++;
+                        totalCaja -= 20;
+                    }
+                    else if (totalCaja % 10 == 0)
+                    {
+                        numBilletes10++;
+                        totalCaja -= 10;
+                    }
+                    else if (totalCaja % 5 == 0)
+                    {
+                        numBilletes5++;
+                        totalCaja -= 5;
+                    }
+                    else if (totalCaja % 1 == 0)
+                    {
+                        numMonedas1++;
+                        totalCaja -= 1;
+                    }
+                    else if (totalCaja % 0.50 == 0)
+                    {
+                        numMonedas50c++;
+                        totalCaja -= 0.50;
+                    }
+                    else if (totalCaja % 0.25 == 0)
+                    {
+                        numMonedas25c++;
+                        totalCaja -= 0.25;
+                    }
+                    else if (totalCaja % 0.10 == 0)
+                    {
+                        numMonedas10c++;
+                        totalCaja -= 0.10;
+                    }
+                    else if (totalCaja % 0.5 == 0)
+                    {
+                        numMonedas5c++;
+                        totalCaja -= 0.5;
+                    }
+
+
+                } while (totalCaja != 0);
+
+                if (numBilletes200 != 0) {
+                    Console.WriteLine("hay " + numBilletes200 + " de billetes de Q200: ");
+                }
+                if (numBilletes100 != 0)
+                {
+                    Console.WriteLine("hay " + numBilletes100 + " de billetes de Q100: ");
+                }
+                if (numBilletes50 != 0)
+                {
+                    Console.WriteLine("hay " + numBilletes50 + " de billetes de Q50: ");
+                }
+                if (numBilletes20 != 0)
+                {
+                    Console.WriteLine("hay " + numBilletes20 + " de billetes de Q20: ");
+                }
+                if (numBilletes10 != 0)
+                {
+                    Console.WriteLine("hay " + numBilletes10 + " de billetes de Q10: ");
+                }
+                if (numBilletes5 != 0) { 
+                    Console.WriteLine("hay " + numBilletes5 + " de billetes de Q5: ");
+                }
+                if (numMonedas1 != 0)
+                {
+                    Console.WriteLine("hay " + numMonedas1 + " de Monedas de Q1: ");
+                }
+                if (numMonedas50c != 0)
+                {
+                    Console.WriteLine("hay " + numMonedas50c + " de Monedas de 50c: ");
+                }
+                if (numMonedas25c != 0)
+                {
+                    Console.WriteLine("hay " + numMonedas25c + " de Monedas de 25c: ");
+                }
+                if (numMonedas10c != 0)
+                {
+                    Console.WriteLine("hay " + numMonedas10c + " de Monedas de 10c: ");
+                }
+                if (numMonedas5c != 0)
+                {
+                    Console.WriteLine("hay " + numMonedas5c + " de Monedas de 5c: ");
+                }
+            }
+            else
+            {
+                goto inicio;
+            }
+
+        }
+    }
+}
---
+++ Semana 8/Intro Progra, Angel Pérez/Intro Progra, Angel Pérez/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Intro_Progra__Angel_Pérez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Daniel Ruano 1247723
+        //Angel Pérez 1135323
+        inicio:
+            Console.Clear();
+            Console.Write("Ingrese el número de empleados: ");
+            bool ni = int.TryParse(Console.ReadLine(), out int numEmpleados);
+            if (ni == true && numEmpleados >= 0)
+            {
+                double pagoTotal = 0;
+
+                for (int i = 1; i <= numEmpleados; i++)
+                {
+                    Console.WriteLine("Empleado #" + i);
+                    Console.Write("Ingrese el número de días trabajados: ");
+                    int numDias = int.Parse(Console.ReadLine());
+
+                    double horasSemana = 0;
+
+                    for (int j = 1; j <= numDias; j++)
+                    {
+                        Console.Write("Ingrese las horas trabajadas en el día " + j + ": ");
+                        double horasDia = double.Parse(Console.ReadLine());
+
+                        horasSemana += horasDia;
+                    }
+
+                    Console.Write("Ingrese el salario por hora del empleado: ");
+                    double salarioHora = double.Parse(Console.ReadLine());
+
+                    double sueldoSemana = horasSemana * salarioHora;
+
+                    Console.WriteLine("El sueldo semanal del empleado #" + i + " es de: Q" + sueldoSemana);
+                    pagoTotal += sueldoSemana;
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("El pago total de los es" + numEmpleados + "y se pago por los empleado: Q" + pagoTotal);
+            }
+            else
+            {
+                goto inicio;
+            }
+
+        }
+    }
+}
+
